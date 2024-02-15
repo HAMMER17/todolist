2bca69e56bb22b10a3af9b3c49ee4b6dfa448d94
HAMMER17 <plutalov70@gmail.com>
2024-02-15 19:51:36 +0300

    localstorage

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const task = document.querySelector('#task')
 const empty = document.querySelector('.empty_list')
 
 let allTasks = []
+if (localStorage.getItem('tasks')) {
+  allTasks = JSON.parse(localStorage.getItem('tasks'))
+}
+allTasks.forEach(element => {
+  const classCss = element.done ? 'done' : 'p'
+  //форма задачи
+  const taskText = ` <li class='li' id='${element.id}'>
+      <input type="checkbox" class="checkbox" data-action='done'>
+    <p class=${classCss}>${element.text} </p>
+    <button data-action='delete'>удалить</button>
+</li>`
+  //  Выводим список на страницу
+  task.insertAdjacentHTML('beforeend', taskText)
+});
+showEmptyList()
 //функция по выводу списка
 form.addEventListener('submit', function (e) {
   e.preventDefault()
@@ -20,6 +35,7 @@ form.addEventListener('submit', function (e) {
   }
   // добавляем задачи в масссив
   allTasks.push(newTask)
+  localStorage.setItem('tasks', JSON.stringify(allTasks))
   //формируем css 
   const classCss = newTask.done ? 'done' : 'p'
   //форма задачи
@@ -34,9 +50,10 @@ form.addEventListener('submit', function (e) {
   input.value = ''
   input.focus()
   //убираем пустой список
-  if (task.children.length > 0) {
-    empty.classList.add('none')
-  }
+  showEmptyList()
+  // if (task.children.length > 0) {
+  //   empty.classList.add('none')
+  // }
 });
 
 //удаление задачи
@@ -53,12 +70,12 @@ task.addEventListener('click', function (e) {
   // const elemId = allTasks.filter((index) => index.id !== id)
   //удаляем из массива задачу
   allTasks.splice(elemId, 1)
-
+  localStorage.setItem('tasks', JSON.stringify(allTasks))
   parentNode.remove()
-
-  if (task.children.length === 0) {
-    empty.classList.remove('none')
-  }
+  showEmptyList()
+  // if (task.children.length === 0) {
+  //   empty.classList.remove('none')
+  // }
 
 });
 //выполнение задания отмечаем
@@ -71,6 +88,22 @@ task.addEventListener('click', function (e) {
   //меняем false на true
   tasks.done = !tasks.done
 
+  localStorage.setItem('tasks', JSON.stringify(allTasks))
+
   const p = parentNode.querySelector('.p')
   p.classList.toggle('done')
 });
+
+function showEmptyList() {
+  if (allTasks.length === 0) {
+    const emptyList = ` <div class="empty_list">
+    <img src="/images/1.png" alt="todo" class="img">
+    <h1>Ваш список пуст</h1>
+  </div>`
+    task.insertAdjacentHTML('afterbegin', emptyList)
+  }
+  if (allTasks.length > 0) {
+    const emptyEl = document.querySelector('.empty_list')
+    emptyEl ? emptyEl.remove() : null;
+  }
+}
